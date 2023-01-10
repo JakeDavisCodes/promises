bc1a791eda1101a79ad3197d51cbda450cdc7618
Jake <jakedavisemail@gmail.com>
2023-01-10 11:58:33 -0700

    Finished up to and including promisification.js

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,42 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, cb) {
+  /** GOALS
+   *
+   * read file
+   * return only the first line
+   *
+   * Psuedo:
+   * I: FilePath
+   * O: Line of a file as a string
+   *
+   * var line;
+   * readFile(filePath, utf8, (file) => {
+   *  line = file.split(/\r?\n/)[0]
+   * }
+   */
+
+  fs.readFile(filePath, 'utf8', (err, file) => {
+    if (err) {
+      cb(err);
+    } else {
+      let line = file.split(/\r?\n/)[0];
+      cb(null, line);
+    }
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, cb) {
+  request('get', url, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,29 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((res, rej) => {
+    var line;
+    let file = fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(data.split(/\r?\n/)[0]);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise((res, rej) => {
+    request(url, (err, response) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(response.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
